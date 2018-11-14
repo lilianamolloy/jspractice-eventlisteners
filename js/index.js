9ee0af1b86f3123a2f4c269ff76b4715808e55aa
Lili Molloy <liliana.molloy@gmail.com>
2018-11-15 10:52:51 +1100

    changed the event listener to be on the parent div

--- js/index.js
+++ js/index.js
@@ -11,20 +11,28 @@ const outterContainer = document.querySelector('.outter-container')
 const oneLine = document.querySelector('.container')
 outterContainer.innerHTML = oneLine.outerHTML.repeat(67)
 
-// lines 15 - 25 sets the event listener changes the background colour
-const boxes = document.querySelectorAll('.box')
+// lines 15 - 23 sets the event listener changes the background colour on click
+// const boxes = document.querySelectorAll('.box')
 
-for (var i = 0; i < boxes.length; i++) {
-    // option1 = changes colour on hover
-    // boxes[i].addEventListener('pointerover', function(e) {
-    //     e.target.style.backgroundColor = 'rgb(152, 247, 239)';
-    // })
-    // boxes[i].addEventListener('pointerover', function(e) {
-    //     e.target.style.border = '1px solid rgb(138, 226, 219)';
-    // })
+// for (var i = 0; i < boxes.length; i++) {
 
-    // option 2 = changes colour onclick
-    boxes[i].addEventListener('click', function(e) {
-        e.target.style.backgroundColor = 'purple';
-    })
+//     // option 2 = changes colour onclick
+//     // boxes[i].addEventListener('click', function(e) {
+//     //     e.target.style.backgroundColor = 'purple';
+//     // })
+// }
+
+// lines 26 - 38 sets event listener on the parent div for efficiency on hover
+const parent = document.querySelector('.outter-container')
+
+parent.addEventListener('mouseover', changeColor)
+
+function changeColor(e) {
+  if(e.target.classList.contains('box')) {
+    const box = e.target
+    box.classList.add('color')
+    setTimeout(function() {
+        box.classList.remove('color')
+    }, 5000)
+  }
 }
